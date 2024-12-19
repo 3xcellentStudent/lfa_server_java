25152a43cfd6bfcd986adbc2a9ee3a2c63e9b8bf
3xcellentStudent <grelaxman2000@gmail.com>
2024-12-18 22:01:24 -0700

    Changed "Product" data model. Improved "ProductsController" and types corrected

--- src/main/java/com/server/services/mongodb/controllers/products/ProductsController.java
+++ src/main/java/com/server/services/mongodb/controllers/products/ProductsController.java
@@ -21,49 +21,90 @@ import com.server.services.mongodb.repositories.products.ProductsRepository;
 @RequestMapping("/mongodb-products")
 @CrossOrigin("*")
 public class ProductsController {
-
+  
   @Autowired
   private ProductsRepository repository;
-  
+
   @PostMapping("/add")
-  public ResponseEntity<ProductsModel> add(@RequestBody ProductsModel product){
-    ProductsModel savedProduct = repository.save(product);
-    return ResponseEntity.ok().body(savedProduct);
+  public ResponseEntity<Object> add(@RequestBody ProductsModel product){
+    try {
+      ProductsModel savedProduct = repository.save(product);
+      return ResponseEntity.ok().body(savedProduct);
+    } catch(Exception error){
+      error.printStackTrace();
+      return ResponseEntity.internalServerError().body("Can't add new product to database !");
+    }
   }
 
   @PutMapping("/update/{id}")
-  public ResponseEntity<ProductsModel> updateById(@PathVariable String id, @RequestBody ProductsModel product){
-    boolean existingDocument = repository.existsById(id);
-    if(existingDocument){
-      product.setId(id);
-      ProductsModel updatedProduct = repository.save(product);
-      return ResponseEntity.ok().body(updatedProduct);
-    } else 
-    return ResponseEntity.notFound().build();
+  public ResponseEntity<Object> updateById(@PathVariable String id, @RequestBody ProductsModel product){
+    try {
+      boolean existingDocument = repository.existsById(id);
+      if(existingDocument){
+        product.setId(id);
+        ProductsModel updatedProduct = repository.save(product);
+        return ResponseEntity.ok().body(updatedProduct);
+      } else return ResponseEntity.status(404).body("Unable to update because this product does not exist !");
+    } catch(Exception error){
+      error.printStackTrace();
+      return ResponseEntity.internalServerError().body("Unable to update this product at database !");
+    }
   }
   
   @GetMapping("/get/{id}")
-  public ResponseEntity<ProductsModel> getById(@PathVariable String id){
-    return ResponseEntity.ok().body(repository.findById(id).get()); 
+  public ResponseEntity<Object> getById(@PathVariable String id){
+    try {
+      Optional<ProductsModel> product = repository.findById(id);
+      if(product.isPresent()){
+        return ResponseEntity.ok().body(product.get()); 
+      } else {
+        return ResponseEntity.notFound().build();
+      }
+    } catch(Exception error){
+      error.printStackTrace();
+      return ResponseEntity.internalServerError().body("Unable get product by ID from database !");
+    }
   }
 
   @GetMapping("/get")
-  public ResponseEntity<List<ProductsModel>> get(){
-    return ResponseEntity.ok().body(repository.findAll()); 
+  public ResponseEntity<Object> get(){
+    try {
+      List<ProductsModel> products = repository.findAll();
+      if(products.isEmpty() == false){
+        return ResponseEntity.ok().body(products); 
+      } else {
+        return ResponseEntity.notFound().build();
+      }
+    } catch(Exception error){
+      error.printStackTrace();
+      return ResponseEntity.internalServerError().body("Can't get all products from database !");
+    }
   }
 
   @GetMapping("/delete/{id}")
-  public ResponseEntity<ProductsModel> deleteById(@PathVariable String id){
-    Optional<ProductsModel> existingDocument = repository.findById(id);
-    if(existingDocument.isPresent()){
-      repository.deleteById(id);
-      return ResponseEntity.ok().body(existingDocument.get());
-    } else return ResponseEntity.notFound().build();
+  public ResponseEntity<Object> deleteById(@PathVariable String id){
+    try {
+      Optional<ProductsModel> existingDocument = repository.findById(id);
+      if(existingDocument.isPresent()){
+        repository.deleteById(id);
+        return ResponseEntity.ok().body(existingDocument.get());
+      } else {
+        return ResponseEntity.notFound().build();
+      }
+    } catch(Exception error){
+      error.printStackTrace();
+      return ResponseEntity.internalServerError().body("Can't delete product by ID from database !");
+    }
   }
 
   @GetMapping("/delete")
   public ResponseEntity<String> delete(){
-    repository.deleteAll();
-    return ResponseEntity.ok().build();
+    try {
+      repository.deleteAll();
+      return ResponseEntity.ok().body("Products have been removed from database !");
+    } catch(Exception error){
+      error.printStackTrace();
+      return ResponseEntity.internalServerError().body("Can't delete products from database !");
+    }
   }
 }
--- src/main/java/com/server/services/mongodb/models/products/ProductsModel.java
+++ src/main/java/com/server/services/mongodb/models/products/ProductsModel.java
@@ -2,196 +2,74 @@ package com.server.services.mongodb.models.products;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.List;
-import java.util.Map;
 
 @Document
 public class ProductsModel {
 
   @Id
   public String id;
-  public Common common;
-  public SectionTitle sectionTitle;
-  public SectionDescr sectionDescr;
-  public SectionDetails sectionDetails;
-  public SectionReviews sectionReviews;
+  public Theme theme;
+  public double rating;
+  public Reviews reviews;
+  public Category category;
+  public String title;
+  public Descriptions descriptions;
+  public StockInfo stockInfo;
+  public List<ProductOption> productOptions;
+  public MediaContent mediaContent;
 
   public ProductsModel(){}
+  
   public ProductsModel(ProductsModel product){
-    this.common = product.common;
-    this.sectionTitle = product.sectionTitle;
-    this.sectionDescr = product.sectionDescr;
-    this.sectionDetails = product.sectionDetails;
-    this.sectionReviews = product.sectionReviews;
+    this.theme = product.theme;
+    this.rating = product.rating;
+    this.reviews = product.reviews;
+    this.category = product.category;
+    this.title = product.title;
+    this.descriptions = product.descriptions;
+    this.stockInfo = product.stockInfo;
+    this.productOptions = product.productOptions;
+    this.mediaContent = product.mediaContent;
   }
 
-  public void setId(String id) {
+  public String setId(String id){
     this.id = id;
-  }
-
-  public String getId() {
     return id;
   }
 
-  public Common getCommon() {
-    return common;
-  }
-
-  public void setCommon(Common common) {
-    this.common = common;
-  }
-
-  public SectionTitle getSectionTitle() {
-    return sectionTitle;
-  }
-
-  public void setSectionTitle(SectionTitle sectionTitle) {
-    this.sectionTitle = sectionTitle;
-  }
-
-  public SectionDescr getSectionDescr() {
-    return sectionDescr;
-  }
-
-  public void setSectionDescr(SectionDescr sectionDescr) {
-    this.sectionDescr = sectionDescr;
-  }
-
-  public SectionDetails getSectionDetails() {
-    return sectionDetails;
-  }
-
-  public void setSectionDetails(SectionDetails sectionDetails) {
-    this.sectionDetails = sectionDetails;
-  }
-
-  public SectionReviews getSectionReviews() {
-    return sectionReviews;
-  }
-
-  public void setSectionReviews(SectionReviews sectionReviews) {
-    this.sectionReviews = sectionReviews;
-  }
-  
-  public static class Common {
-    public double rating;
-    public Map<String, String> category;
-    public String title;
-    public Theme theme;
-    public String productLogo;
-    public SxQuantity sxQuantity;
-  }
-
   public static class Theme {
-    public Map<String, String> colors;
-    public Map<String, SxCircle> shadows;
+    public Colors colors;
+
+    public static class Colors {
+      public TextColors text;
+      public BackgroundColors backgrounds;
+
+      public static class TextColors {
+        public ColorTypes primary;
+        public ColorTypes secondary;
+        public ColorTypes optional;
+      }
+      public static class BackgroundColors {
+        public ColorTypes primary;
+        public ColorTypes secondary;
+        public ColorTypes optional;
+        public ColorTypes elementsPrimary;
+        public ColorTypes elementsSecondary;
+        public ColorTypes elementsOptional;
+      }
+      public static class ColorTypes {
+        public String hex;
+        public String rgb;
+      }
+    }
   }
 
-  public static class SxCircle {
-    public String boxShadow;
-  }
-
-  public static class SxQuantity {
-    public String boxShadow;
-    public String backgroundColor;
-    public String color;
-    public Hover hover;
-  }
-
-  public static class Hover {
-    public String backgroundColor;
-    public String color;
-    public String boxShadow;
-  }
-
-  public static class SectionTitle {
-    public String description;
-    public double price;
-    public int quantityMax;
-    public Map<String, String> sx;
-    public PurchasePart purchasePart;
-  }
-
-  public static class PurchasePart {
-    public SxCont sxCont;
-    public Components components;
-  }
-
-  public static class SxCont {
-    public String color;
-    public String backgroundColor;
-  }
-
-  public static class Components {
-    public TitleC titleC;
-    public AddCartC addCartC;
-    public SelectionC selectionC;
-  }
-
-  public static class TitleC {
-    public RatingC ratingC;
-  }
-
-  public static class RatingC {
-    public String content;
-    public Map<String, String> sxText;
-    public Map<String, String> sxRating;
-  }
-
-  public static class AddCartC {
-    public Map<String, Object> sxBtn;
-  }
-
-  public static class SelectionC {
-    public Map<String, Object> sxBox;
-    public List<FieldC> fieldC;
-  }
-
-  public static class FieldC {
-    public Map<String, String> sxField;
-    public String name;
-    public String type;
-    public List<Item> items;
-  }
-
-  public static class Item {
-    public String value;
-    public String fill;
-    public String stroke;
-  }
-
-  public static class SectionDescr {
-    public Map<String, Object> sx;
-    public List<List<Image>> images;
-    public List<String> description;
-  }
-
-  public static class Image {
-    public String media;
-    public String src;
-  }
-
-  public static class SectionDetails {
-    public Map<String, Object> sx;
-    public List<DetailArray> array;
-  }
-
-  public static class DetailArray {
-    public String title;
-    public List<DetailItem> items;
-  }
-
-  public static class DetailItem {
-    public String name;
-    public String value;
-  }
-
-  public static class SectionReviews {
+  public static class Reviews {
+    public int countOfReviews;
     public ReviewsSnapshot reviewsSnapshot;
-    public Map<String, Object> sxRating;
-    public Map<String, Object> sxText;
-    public Map<String, Object> sxFilter;
-    public UserReviews userReviews;
+    public List<Review> reviewsList;
   }
 
   public static class ReviewsSnapshot {
@@ -202,15 +80,51 @@ public class ProductsModel {
     public int one;
   }
 
-  public static class UserReviews {
-    public Theme theme;
-    public List<Review> reviewsArray;
-  }
-
   public static class Review {
     public String name;
     public String text;
-    public int rating;
+    public String rating;
     public List<String> attachments;
+  }
+
+  public static class Category {
+    public String name;
+  }
+
+  public static class Descriptions {
+    public String summary;
+    public String[] presentable;
+  }
+
+  public static class StockInfo {
+    public int quantityMax;
+    public double price;
+  }
+
+  public static class ProductOption {
+    public String name;
+    public String type;
+    public List<Item> items;
+  }
+
+  public static class Item {
+    public String value;
+    public String fill;
+    public String stroke;
+    public boolean stockStatus;
+  }
+
+  public static class MediaContent {
+    public TitleContent titleContent;
+    public List<List<Image>> images;
+  }
+
+  public static class TitleContent {
+    public String productLogo;
+  }
+
+  public static class Image {
+    public String media;
+    public String src;
   }
 }
